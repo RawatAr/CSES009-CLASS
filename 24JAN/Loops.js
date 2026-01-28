a826095045e24bff52285d5079b4c9daea90eac2
Aryan <rwtaryan07@gmail.com>
2026-01-28 21:46:14 +0530

    adding assignment

---
+++ 24JAN/Loops.js
@@ -0,0 +1,41 @@
+// for( i=1; i<=10; i++){
+//     if(i%2==0){
+//         console.log(i);
+//     }
+// };
+// var num = 10;
+// while(num<=10){
+//     console.log(i);
+// }
+
+// var num = 0;
+// while(num<=10){
+//     console.log(num);
+//     num++;
+// }
+
+// var num = 0;
+// while(num<=10){
+//     if(num%2==0){
+//         console.log(num);
+//         // num++;
+//     }
+//     num++;
+// }
+
+// let arr = [10,20,30,40,50];
+// for(let value of arr){
+//     console.log(value);
+// }
+
+let obj = {name:"Aryan",age:20};
+
+for(let key in obj){
+    console.log(key);
+}                                // let obj = {key:value};
+
+                                // for(let key in obj){
+                                //     console.log(key);
+                                // }
+
+
---
+++ 24JAN/script.js
@@ -0,0 +1,126 @@
+// // // var name = "Aryan";
+// // // let age = "20";
+// // // const city = "Jalandhar";
+// // // // console.log(name);
+// // // // console.log(age);
+// // // // console.log(city);
+
+// // // console.log(typeof(age));
+
+// // // let a = 25;
+// // // let name = "Aryan Rawat";
+// // // let isStudent = true;
+// // // let x;
+// // // let y = null;
+
+// // // console.log(a);
+// // // console.log(name);
+// // // console.log(isStudent);
+// // // console.log(x);
+// // // console.log(y);
+
+// // // console.log(typeof(a));             //number
+// // // console.log(typeof(name));          //string  
+// // // console.log(typeof(isStudent));     //boolean  
+// // // console.log(typeof(x));             //undedined
+// // // console.log(typeof(y));             //object
+
+
+// // //ARTHEMATIC OPERATIONS
+// // // let num01 = 6;
+// // // let num02 = 10;
+// // // let char = "18";                             //NAN - NOT A NUMBER ("abc"/6) we get "NAN error"; 
+// //                                             // BUT ("18"/6) we get 3   because it convert it;
+                                            
+
+// // // console.log("Addition:",num01 + num02);
+// // // console.log("Substraction:",num01 - num02);
+// // // console.log("Multiply:",num01 * num02);
+// // // console.log("Division:",num01 / num02);
+// // // console.log(num01 , num02);
+// // // console.log(char / num01);
+
+// // //NUMBER CONVERSION
+
+// // // let numb = 10;
+// // // console.log(Number(numb));
+// // // console.log(parseInt("20"));
+// // // console.log(parseFloat("3.5"));
+// // // console.log(typeof(numb));
+
+
+// // //STRING CONCATENATION
+
+// // // let s1 = "a ";
+// // // let s2 = "b ";
+// // // let s3 = "c ";
+// // // console.log(s1+s2+s3);
+
+// // // let a = "My";
+// // // let b = "name";
+// // // let c = "is";
+// // // let d = "Aryan";
+// // // let e = "Rawat.";
+// // // console.log(a + " "+ b + " " + c + " " + d + " " + e);
+// // // console.log(d.length);
+// // // console.log(d.toUpperCase());
+// // // console.log(d.toLowerCase());
+
+// // // const arr = ["apple", "banana", "grapes", "orange", "cherry"];
+// // // console.log(arr.slice(4));
+
+
+// // //TEMPLATE LITERALS
+// // // let name = "Aryan";
+// // // let age = 20;
+
+// // // console.log(`Hey there my name is ${name} and age ${age}.`);
+
+// // // let z = 10;
+
+// // // function test(){
+// // //     let f =7;
+// // //     console.log(z);
+// // //     console.log(f);
+// // // }
+// // // test();
+// // // console.log(z);
+// // // console.log(f);
+
+
+// // // let num1 = 10;
+
+// // // function test(){
+// // //     let num2 =20;
+// // //     console.log(num1);
+// // //     console.log(num2);
+// // // }
+// // // test();
+// // // console.log(num1);
+// // // console.log(num2);
+
+
+// // if(true){
+// //     var x= 20;
+// //     let y = 22;
+// // }
+// // console.log(x);
+// // console.log(y);
+
+
+
+// // console.log(a);    //let a=5 refrence error; var a=5 undefined 
+// // let a = 10;
+
+// // console.log("5"=== 5); //strict type checking
+// // console.log("5"== 5); //loose type checking
+// // console.log(0 == false);
+// // console.log(null == undefined); 
+// // console.log("" == null);
+// // console.log([] == 0);
+
+
+// console.log([] === 0);
+// console.log(0 === false);
+// console.log(null === undefined); 
+// console.log("" === null);
